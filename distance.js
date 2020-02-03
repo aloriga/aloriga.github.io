fb554faa71408a74f21c930ea5b7acabfbd923d3
aloriga <55288010+aloriga@users.noreply.github.com>
2020-02-03 04:21:26 -0500

    Update distance.js

--- distance.js
+++ distance.js
@@ -10,9 +10,11 @@ function toFixed(){
   document.getElementById("orderPair1").innerHTML= "(" + X1 + " , " + Y1 +" )";
   document.getElementById("orderPair2").innerHTML= "(" + X2 + " , " + Y2 +" )";
     var distance = Math.sqrt(Math.pow(Math.abs(Y2-Y1),2)+Math.pow(Math.abs(X2-X1),2));
-  
- document.getElementById("distance").innerHTML= distance;
+  distance = Math.round(distance*100)/100;
+ document.getElementById("distance").innerHTML= "The distance to two decimal places is :" + distance;
+  document.getElementById("distance2").innerHTML= "You should not travel more than: " + Math.floor(distance);
 }
+
 function toStart(){
   document.getElementById("orderPair1").innerHTML= "(" +  " , "  +" )";
   document.getElementById("orderPair2").innerHTML= "(" +  " , "  +" )";
